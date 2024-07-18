9c88e3d372e5e1b7cd5883a10b8d3d5373d76109
Gergely Sabli <139555565+sabligeri@users.noreply.github.com>
2024-07-18 22:59:16 +0200

    Initial commit

---
+++ src/test/java/com/storage/storagemonitorbackend/StorageMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storage.storagemonitorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageMonitorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
